df35decc4c07119635e3b237b6e7966d17993273
Josue Soto <jsoto.consultant@gmail.com>
2025-07-18 00:02:14 -0600

    Tunneling working still with hardcoded ports but all connections forwarding to internal tunnel

--- client/tunnel.go
+++ client/tunnel.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
 
 func openTunnel() {
-	fmt.Println("Opening tunnel (placeholder)")
+	serverConn, err := net.Dial("tcp", "localhost:9091")
+	if err != nil {
+		log.Println("Failed to connect to tunnel port:", err)
+		return
+	}
+	defer serverConn.Close()
+
+	fmt.Println("Tunnel connection established")
+
+	localConn, err := net.Dial("tcp", "localhost:3000")
+	if err != nil {
+		log.Println("Failed to connect to local service:", err)
+		serverConn.Close()
+		return
+	}
+	defer localConn.Close()
+
+	fmt.Println("Local service connection established")
+
+	go io.Copy(localConn, serverConn)
+	io.Copy(serverConn, localConn)
 }
